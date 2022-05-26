fb78bbff44549ce1f81e4508b24613cd8c0fc041
GrayXcode <hoyx0101@gmail.com>
2022-05-26 12:22:33 +0100

    ADD: add image folder to public

--- disney-clone/src/components/Login.js
+++ disney-clone/src/components/Login.js
@@ -4,14 +4,19 @@ import styled from "styled-components";
 function Login() {
   return (
     <Container>
-      <Content>Content</Content>
+      <Content>
+        <CTA>
+          <CTALogoOne src="/images/cta-logo-one.svg" />
+        </CTA>
+        <BgImage />
+      </Content>
     </Container>
   );
 }
 
 export default Login;
 
-const Container = styled.div`
+const Container = styled.section`
   overflow: hidden;
   display: flex;
   flex-direction: column;
@@ -20,12 +25,31 @@ const Container = styled.div`
 `;
 
 const Content = styled.div`
-    margin-bottom: 10vw;
-    width: 100%;
-    position: relative;
-    min-height: 100vh;
-    box-sizing: border-box;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  margin-bottom: 10vw;
+  width: 100%;
+  position: relative;
+  min-height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 80px 40px;
+  height: 100%;
 `;
+
+const BgImage = styled.div`
+  height: 100%;
+  background-image: url("/images/login-background.jpg");
+  background-position: top;
+  background-size: cover;
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
+`;
+
+const CTA = styled.div``
+
+const CTALogoOne = styled.img``
